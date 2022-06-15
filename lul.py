f6e83d712d63b78ca15ae74be9f8f5f021429834
maybe2105 <luannguyen210500@gmail.com>
2022-06-16 00:29:38 +0700

    socket

---
+++ lul.py
@@ -0,0 +1,44 @@
+from binance import AsyncClient, BinanceSocketManager
+import asyncio
+import time
+
+import pandas as pd
+
+from binance import ThreadedWebsocketManager
+from binance.client import Client
+api_key = 'NwwA7xRmAq0juYxurtfqmAiW7asFDxB33zQknnaOEmEItWnbR0bVVtjoZLV6tQBy'
+api_secret = 'Lqq5JzhBANnszyHblQwRhKDgrPjfmjDGdcZA7BZdg3e7pJZkir5vRnBTQh8k4ypp'
+apiClient = Client(api_key, api_secret)
+
+
+df = pd.DataFrame(apiClient.get_historical_klines(
+    "BTCUSDT", Client.KLINE_INTERVAL_1MINUTE))
+df = df.iloc[:, :6]
+df.columns = ["Date", "Open", "High", "Low", "Close", "Volume"]
+df = df.set_index("Date")
+df.index = pd.to_datetime(df.index, unit="ms")
+df = df.astype("float")
+
+print(df.values.tolist())
+# print(call)
+
+# data = []
+
+
+# async def a():
+#     client = await AsyncClient.create()
+#     bm = BinanceSocketManager(client)
+#     # start any sockets here, i.e a trade socket
+#     ts = bm.symbol_miniticker_socket("btcusdt")
+#     # then start receiving messages
+#     async with ts as tscm:
+#         while True:
+#             res = await tscm.recv()
+#             data.append(res)
+#             print(len(data))
+
+#     await client.close_connection()
+
+
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(a())
