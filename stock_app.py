40ba2d29947772b0be29a4331a844fbc6c8e0852
maybe2105 <luannguyen210500@gmail.com>
2022-07-05 22:41:23 +0700

    fix stock app

--- stock_app.py
+++ stock_app.py
@@ -7,49 +7,69 @@ from dash.dependencies import Input, Output
 from keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
+
+
 app = dash.Dash()
 server = app.server
+
 scaler = MinMaxScaler(feature_range=(0, 1))
-df_nse = pd.read_csv("./GOOG.csv")
+
+df_nse = pd.read_csv("./NSE-TATA.csv")
+
 df_nse["Date"] = pd.to_datetime(df_nse.Date, format="%Y-%m-%d")
 df_nse.index = df_nse['Date']
+
+
 data = df_nse.sort_index(ascending=True, axis=0)
 new_data = pd.DataFrame(index=range(0, len(df_nse)), columns=['Date', 'Close'])
+
 for i in range(0, len(data)):
     new_data["Date"][i] = data['Date'][i]
     new_data["Close"][i] = data["Close"][i]
+
 new_data.index = new_data.Date
 new_data.drop("Date", axis=1, inplace=True)
+
 dataset = new_data.values
-train = dataset[0:227, :]
-valid = dataset[227:, :]
+
+train = dataset[0:987, :]
+valid = dataset[987:, :]
+
 scaler = MinMaxScaler(feature_range=(0, 1))
 scaled_data = scaler.fit_transform(dataset)
+
 x_train, y_train = [], []
+
 for i in range(60, len(train)):
     x_train.append(scaled_data[i-60:i, 0])
     y_train.append(scaled_data[i, 0])
 
 x_train, y_train = np.array(x_train), np.array(y_train)
+
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-model = load_model("saved_rnn.h5")
+
+model = load_model("saved_model.h5")
 
 inputs = new_data[len(new_data)-len(valid)-60:].values
-print("findme:", len(inputs), inputs.shape[0])
-
 inputs = inputs.reshape(-1, 1)
 inputs = scaler.transform(inputs)
+
 X_test = []
-for i in range(60, 227):
+for i in range(60, inputs.shape[0]):
     X_test.append(inputs[i-60:i, 0])
 X_test = np.array(X_test)
+
 X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
 closing_price = model.predict(X_test)
 closing_price = scaler.inverse_transform(closing_price)
+
 train = new_data[:987]
 valid = new_data[987:]
 valid['Predictions'] = closing_price
+
+
 df = pd.read_csv("./stock_data.csv")
+
 app.layout = html.Div([
 
     html.H1("Stock Price Analysis Dashboard", style={"textAlign": "center"}),
@@ -68,6 +88,7 @@ app.layout = html.Div([
                                 y=valid["Close"],
                                 mode='markers'
                             )
+
                         ],
                         "layout":go.Layout(
                             title='scatter plot',
@@ -75,6 +96,7 @@ app.layout = html.Div([
                             yaxis={'title': 'Closing Rate'}
                         )
                     }
+
                 ),
                 html.H2("LSTM Predicted closing price",
                         style={"textAlign": "center"}),
@@ -87,6 +109,7 @@ app.layout = html.Div([
                                 y=valid["Predictions"],
                                 mode='markers'
                             )
+
                         ],
                         "layout":go.Layout(
                             title='scatter plot',
@@ -94,8 +117,11 @@ app.layout = html.Div([
                             yaxis={'title': 'Closing Rate'}
                         )
                     }
+
                 )
             ])
+
+
         ]),
         dcc.Tab(label='Facebook Stock Data', children=[
             html.Div([
@@ -125,6 +151,8 @@ app.layout = html.Div([
                 dcc.Graph(id='volume')
             ], className="container"),
         ])
+
+
     ])
 ])
 
